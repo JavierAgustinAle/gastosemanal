b3809331d9765f4c057e45e4f17b686621eef5fd
JavierAle <javierale25@live.com>
2020-07-14 21:11:29 -0300

    Creacion del proyecto y commit inicial

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+import Header from './Header';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div className="App container">
+          <Header
+            titulo='Gasto Semanal'
+          />
+          <div className="contenido-principal contenido">
+            <div className="row">
+              <div className="one-half column">
+
+              </div>
+              <div className="one-half column">
+
+              </div>
+            </div>
+          </div>
+
+
+
+
+        </div>
+      </>
+    );
+  }
+
+}
+
+export default App;
