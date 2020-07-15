1e9c8c6fbaf76807160da2a9d9d5e68d108f0696
JavierAle <javierale25@live.com>
2020-07-14 21:38:12 -0300

    Creo un formulario donde el user ingresara los datos del nombre y del total del gasto

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 import Header from './Header';
+import Formulario from './Formulario';
 
 class App extends Component {
+
+  state = {
+    presupuesto: '',
+    gastos: {},
+    restante: ''
+  }
+
+  //Agregar gasto al state
+  agregarGasto = gasto => {
+    const gastos = { ...this.state.gastos }
+
+    // agregos el gsto al state con la fecha para que no se repitan
+    gastos[`gastos${Date.now()}`] = gasto;
+
+    this.setState({
+      gastos
+    })
+  }
+
   render() {
     return (
       <>
@@ -13,7 +33,9 @@ class App extends Component {
           <div className="contenido-principal contenido">
             <div className="row">
               <div className="one-half column">
-
+                <Formulario
+                  agregarGasto={this.agregarGasto}
+                />
               </div>
               <div className="one-half column">
 
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+
+class Formulario extends Component {
+
+    // refs para campos del form
+    nombreGasto = React.createRef();
+    totalGasto = React.createRef();
+
+    crearGasto = e => {
+        e.preventDefault();
+
+        const gasto = {
+            nombreGasto: this.nombreGasto.current.value,
+            totalGasto: this.totalGasto.current.value
+        }
+
+        this.props.agregarGasto(gasto);
+
+        e.currentTarget.reset();
+    }
+
+
+    render() {
+        return (
+            <form onSubmit={this.crearGasto}>
+                <h2>Agrega tus gastos aqui</h2>
+                <div className="campo">
+                    <label>Nombre Gasto</label>
+                    <input className="u-full-width" type="text" placeholder="Ej. Transporte" ref={this.nombreGasto} />
+                </div>
+
+                <div className="campo">
+                    <label>Cantidad</label>
+                    <input className="u-full-width" type="number" placeholder="Ej. 300" ref={this.totalGasto} />
+                </div>
+
+                <input className="button-primary u-full-width" type="submit" value="Agregar" />
+            </form>
+        )
+    }
+}
+
+export default Formulario;
