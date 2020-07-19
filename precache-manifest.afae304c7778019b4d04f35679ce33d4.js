3d996c5c886f9c550453b5e94e5315112a394650
JavierAle <javierale25@live.com>
2020-07-19 13:35:12 -0300

    Updates

---
+++ precache-manifest.afae304c7778019b4d04f35679ce33d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aabb1818738e4efd60dfeadb31de08ec",
+    "url": "/gastosemanal/index.html"
+  },
+  {
+    "revision": "0e4bbfdc08ba38d72196",
+    "url": "/gastosemanal/static/css/main.88da9434.chunk.css"
+  },
+  {
+    "revision": "fd8cc5865750aa9c3bf3",
+    "url": "/gastosemanal/static/js/2.96e9eb5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gastosemanal/static/js/2.96e9eb5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e4bbfdc08ba38d72196",
+    "url": "/gastosemanal/static/js/main.1c8d313a.chunk.js"
+  },
+  {
+    "revision": "068c1ba5b0a7f7a1381c",
+    "url": "/gastosemanal/static/js/runtime-main.6efc65de.js"
+  }
+]);
